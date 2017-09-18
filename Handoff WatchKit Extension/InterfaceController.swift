255d748b9f8f3b25a7b4e1b65a91b1e5c08dbc27
Joao <joaovitornaru@hotmail.com>
2017-09-18 15:47:58 -0300

    Finalizado.

--- Handoff WatchKit Extension/InterfaceController.swift
+++ Handoff WatchKit Extension/InterfaceController.swift
@@ -15,6 +15,7 @@ class InterfaceController: WKInterfaceController {
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        self.updateUserActivity("br.com.jaum.Handoff", userInfo: nil, webpageURL: nil)
         // Configure interface objects here.
     }
     
